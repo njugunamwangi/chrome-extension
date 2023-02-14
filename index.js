f8cd7cf4bafa5c6f4d974a72c46c848865f9dda0
njugunamwangi <njugunamwangi.n@gmail.com>
2023-02-14 16:12:02 +0300

    Display the leads in a list

---
+++ index.js
@@ -0,0 +1,22 @@
+let myLeads = []
+
+const inputBtn = document.querySelector("#input-btn")
+const inputEl = document.querySelector("#input-el")
+const ulEl = document.querySelector("#ul-el")
+
+inputBtn.addEventListener("click", function () {
+    myLeads.push(inputEl.value)
+    renderLeads()
+}) 
+
+function renderLeads() {
+    let listItems = ""
+    
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems += "<li>"+ myLeads[i] +"</li>"
+        // const li = document.createElement("li")
+        // li.textContent = myLeads[i]
+        // ulEl.append(li)
+    }
+    ulEl.innerHTML = listItems
+}
