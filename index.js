e9a883158fe1dff7d2681c16d3a5a4d215811bcb
njugunamwangi <njugunamwangi.n@gmail.com>
2023-02-14 18:38:00 +0300

    Code refactoring

--- index.js
+++ index.js
@@ -14,7 +14,14 @@ function renderLeads() {
     let listItems = ""
     
     for (let i = 0; i < myLeads.length; i++) {
-        listItems += "<li><a href='//" + myLeads[i] + "' target='_blank'>"+ myLeads[i] +"</a></li>"
+        listItems += `
+            <li>
+                <a href='//${myLeads[i]}' target='_blank'>
+                    ${myLeads[i]}
+                </a>
+            </li>
+        `
+
         // const li = document.createElement("li")
         // li.textContent = myLeads[i]
         // ulEl.append(li)
